d2a59bb7244675255c4e96d34cae41a7bd0b9156
Mateusz Budzyński <budzynekm09@gmail.com>
2024-01-20 19:34:08 +0100

    card class added

---
+++ card.py
@@ -0,0 +1,14 @@
+
+from buttons import Buttons
+
+
+class Card:
+
+    def __init__(self, center_coordinates, graphics, alt_graphics, screen,
+                 lvl, points, color):
+        self.screen = screen
+        self.card_button = Buttons(center_coordinates, graphics, alt_graphics, screen)
+        self.lvl = lvl
+        self.points = points
+        self.cost = {"red": 0, "blue": 0, "black": 0, "white": 0, "green": 0}
+        self.color = color
