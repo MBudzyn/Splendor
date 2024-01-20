a2bf219add8e8ac059a2e1c6d32adcc5e997a359
Mateusz Budzyński <budzynekm09@gmail.com>
2024-01-20 19:04:18 +0100

    basic templates

---
+++ menu_scene.py
@@ -0,0 +1,16 @@
+from scene import *
+class MenuScene(Scene):
+    def __init__(self, screen,scene_man):
+        super().__init__(screen)
+        self.scene_man = scene_man
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def update(self):
+        pass
+    def render(self):
+        pass
---
+++ scene.py
@@ -0,0 +1,13 @@
+import pygame,sys
+class Scene:
+    def __init__(self, screen):
+        self.screen = screen
+    def handle_events(self):
+        pass
+    def update(self):
+        pass
+    def render(self):
+        pass
+
+
+
