27b7bc102dc0f137c6af7e648a875a67f051d171
Mateusz Budzyński <budzynekm09@gmail.com>
2024-01-21 17:38:59 +0100

    display method and modyfications

--- stack_of_tokens.py
+++ stack_of_tokens.py
@@ -1,21 +1,32 @@
 from token import Token
+import pygame
 class StackOfTokens:
-    def __init__(self, color, amount, is_universal=False):
+    def __init__(self, color, amount, screen, is_universal=False):
         self.color = color
         self.amount = amount
         self.is_universal = is_universal
         self.tokens = [Token(color, is_universal) for _ in range(amount)]
+        self.graphics_links = [f"graphics/{color}_token{i}.png" for i in range(8)]
         self.max_amount = 7
+        self.set_image_to_actual()
+        self.screen = screen
 
     def is_empty(self):
         return self.amount == 0
+
+    def set_image_to_actual(self):
+        self.actual_stack_image = pygame.image.load(self.graphics_links[self.amount])
+        self.actual_stack_image_rect = self.actual_stack_image.get_rect(center=(400, 400))
+
+    def display(self, screen):
+        screen.blit(self.actual_stack_image, self.actual_stack_image_rect)
 
     def is_possible_to_take_token(self):
         return not self.is_empty()
 
     def is_possible_to_take_two_tokens(self):
         return self.amount >= 4
-    
+
     def is_possible_to_add_token(self):
         return self.amount < self.max_amount
 
