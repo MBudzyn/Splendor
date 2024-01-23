1e9f7620880532a1f985bf71a4e828781513e171
Mateusz Budzyński <budzynekm09@gmail.com>
2024-01-23 19:37:11 +0100

    point class update

--- point.py
+++ point.py
@@ -1,13 +1,20 @@
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
-        self.coordinates = (x, y)
+        self.coordinates: tuple[int, int] = (x, y)
 
-    def get_coordinates(self):
+    def get_coordinates(self) -> tuple[int, int]:
         return self.coordinates
 
-    def set_coordinates(self, x, y):
+    def set_coordinates(self, x: int, y: int):
         self.x = x
         self.y = y
         self.coordinates = (x, y)
+
+    def increase_coordinates(self, x: int, y: int):
+        self.x += x
+        self.y += y
+        self.coordinates = (self.x, self.y)
+
+
