6ee63db781210d027a6a6492d720c6bd5d6d7c9f
Mateusz Budzyński <budzynekm09@gmail.com>
2024-01-25 10:42:56 +0100

    special token count in stack changed to 5

--- row_of_cards_in_ply.py
+++ row_of_cards_in_ply.py
@@ -14,3 +14,22 @@ class RowOfCards:
     def display(self):
         for _card in self.cards:
             _card.disply()
+
+    def remove_card(self, card: Card):
+        self.cards.remove(card)
+
+    def add_card(self, card: Card):
+        self.cards.append(card)
+
+    def is_possible_to_add_card(self):
+        return len(self.cards) <= 3
+
+    def is_full(self):
+        return len(self.cards) == 4
+
+    def is_possible_to_remove_card(self):
+        return len(self.cards) > 0
+
+    def add_from_top_of_deck(self):
+        if self.deck_of_cards.is_possible_to_remove_card() and self.is_possible_to_add_card():
+            self.add_card(self.deck_of_cards.get_card_from_top())
