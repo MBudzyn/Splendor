a33d1ee576d222ee8bd489a07e81d55e219eb0c5
Mateusz Budzyński <budzynekm09@gmail.com>
2024-01-27 19:41:14 +0100

    color dict class

---
+++ color_dict.py
@@ -0,0 +1,18 @@
+class ColorDict:
+    def __init__(self):
+        self.dict_with_colors = self.cost = {"red": 0, "blue": 0, "black": 0,
+                                             "white": 0, "green": 0, "special": 0}
+
+    def get_dict(self):
+        return self.dict_with_colors
+
+    def increase_color_value(self, color, value):
+        self.dict_with_colors[color] += value
+
+    def set_value(self, color, value):
+        self.dict_with_colors[color] = value
+
+    def increse_by_color_dict(self,second_color_dict):
+        for key, value in second_color_dict.get_dict:
+            self.increase_color_value(key,value)
+
