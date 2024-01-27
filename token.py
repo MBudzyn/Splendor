eb9f58fcfc0a454381c601e0128d0026d191c918
Mateusz Budzyński <budzynekm09@gmail.com>
2024-01-27 20:11:31 +0100

    .

--- token.py
+++ token.py
@@ -1,12 +1,13 @@
 from button import Button
 from point import Point
 
+
 class Token:
     def __init__(self, color, screen, is_universal=False, coordinates=None):
         self.color = color
         self.is_universal = is_universal
         self.coordinates = coordinates
-        self.button = Button(Point(600,600),f"graphics/{color}_token.png",screen)
+        self.button = Button(Point(600, 600), f"graphics/{color}_token.png", screen)
 
     def display(self):
         self.button.display()
