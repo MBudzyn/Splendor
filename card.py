dee3ac7b02137d938113432464bec575835ed22b
Mateusz Budzyński <budzynekm09@gmail.com>
2024-02-19 10:10:00 +0100

    reserved atribute added to card class'

--- card.py
+++ card.py
@@ -4,19 +4,25 @@ from point import Point
 
 class Card:
     def __init__(self, coordinate_point: Point, graphics, alt_graphics, screen,
-                 lvl, points, color, cost = None):
-
+                 lvl, points, color, cost=None):
         self.screen = screen
         self.card_button = Button(coordinate_point, graphics, screen, alt_graphics)
         self.lvl = lvl
         self.points = points
         self.cost = cost
         self.color = color
+        self.is_reserved = False
         if self.cost is None:
             self.cost = {"red": 0, "blue": 0, "black": 0, "white": 0, "green": 0}
 
     def display(self):
         self.card_button.display()
+
+    def set_reserved(self, boolean):
+        self.is_reserved = boolean
+
+    def get_reserved(self):
+        return self.is_reserved
 
     def update(self):
         self.card_button.update()
