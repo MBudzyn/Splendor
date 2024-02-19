52bec0c5f3d2140fd981f76fee585f120d9d2674
Mateusz Budzyński <budzynekm09@gmail.com>
2024-02-19 09:06:57 +0100

    actual player name render on board

--- player.py
+++ player.py
@@ -1,17 +1,22 @@
-
 from player_cards import PlayerCards
 from player_tokens import PlayerTokens
+
+
 class Player:
     def __init__(self, nick_name, screen, action_field):
         self.nick_name = nick_name
         self.screen = screen
         self.player_cards = PlayerCards(action_field)
-        self.player_tokens = PlayerTokens(screen,action_field)
+        self.player_tokens = PlayerTokens(screen, action_field)
         self.is_turn = False
+        import pygame
+        self.font = pygame.font.SysFont("Comic Sans MS", 70)
+        self.text = self.font.render(nick_name, False, [255, 255, 255])
 
     def display(self):
         self.player_cards.display()
         self.player_tokens.display()
+        self.screen.blit(self.text, (900,550))
 
     def set_turn(self, is_turn):
         self.is_turn = is_turn
